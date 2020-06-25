9c7083fbde5fb8c4005836e655e79a516c57ca4c
SaNu-hIT <sanufeliz@gmail.com>
2020-06-25 13:47:14 +0530

    Update operators.js
    update token

--- routes/operators.js
+++ routes/operators.js
@@ -37,7 +37,7 @@ let token = await TokenSchema.find({
 
 if(token)
 {
-  var access_token = "eyJraWQiOiI1N2JjZjNhNy01YmYwLTQ1M2QtODQ0Mi03ODhlMTA4OWI3MDIiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNDU1IiwiaXNzIjoiaHR0cHM6Ly9yZWxvYWRseS1zYW5kYm94LmF1dGgwLmNvbS8iLCJodHRwczovL3JlbG9hZGx5LmNvbS9zYW5kYm94Ijp0cnVlLCJodHRwczovL3JlbG9hZGx5LmNvbS9wcmVwYWlkVXNlcklkIjoiMzQ1NSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF1ZCI6Imh0dHBzOi8vdG9wdXBzLWhzMjU2LXNhbmRib3gucmVsb2FkbHkuY29tIiwibmJmIjoxNTkyOTM1NTE0LCJhenAiOiIzNDU1Iiwic2NvcGUiOiJzZW5kLXRvcHVwcyByZWFkLW9wZXJhdG9ycyByZWFkLXByb21vdGlvbnMgcmVhZC10b3B1cHMtaGlzdG9yeSByZWFkLXByZXBhaWQtYmFsYW5jZSByZWFkLXByZXBhaWQtY29tbWlzc2lvbnMiLCJleHAiOjE1OTMwMjE5MTQsImh0dHBzOi8vcmVsb2FkbHkuY29tL2p0aSI6ImI1NmY4ZmYwLWJlOGEtNGNkMS1iZjIzLTM3ZmYwZmUyYTNhZCIsImlhdCI6MTU5MjkzNTUxNCwianRpIjoiODM3MDdkODEtMGRiNC00ZTVkLTkxNDMtMzE0OWU4NjcyZGY5In0.tDX5DQwyrSmiTYV8L2aMJBSEtabDstWPAGHsk1qej40"
+  var access_token = "eyJraWQiOiI1N2JjZjNhNy01YmYwLTQ1M2QtODQ0Mi03ODhlMTA4OWI3MDIiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNDU1IiwiaXNzIjoiaHR0cHM6Ly9yZWxvYWRseS1zYW5kYm94LmF1dGgwLmNvbS8iLCJodHRwczovL3JlbG9hZGx5LmNvbS9zYW5kYm94Ijp0cnVlLCJodHRwczovL3JlbG9hZGx5LmNvbS9wcmVwYWlkVXNlcklkIjoiMzQ1NSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF1ZCI6Imh0dHBzOi8vdG9wdXBzLWhzMjU2LXNhbmRib3gucmVsb2FkbHkuY29tIiwibmJmIjoxNTkzMDcyOTY5LCJhenAiOiIzNDU1Iiwic2NvcGUiOiJzZW5kLXRvcHVwcyByZWFkLW9wZXJhdG9ycyByZWFkLXByb21vdGlvbnMgcmVhZC10b3B1cHMtaGlzdG9yeSByZWFkLXByZXBhaWQtYmFsYW5jZSByZWFkLXByZXBhaWQtY29tbWlzc2lvbnMiLCJleHAiOjE1OTMxNTkzNjksImh0dHBzOi8vcmVsb2FkbHkuY29tL2p0aSI6IjBmNjE2NzRhLWY4NDAtNGUyMC04YzZkLTc3YjBiN2Y1MmQ1MCIsImlhdCI6MTU5MzA3Mjk2OSwianRpIjoiOGE2OGM0OWYtMGEyNy00MzE4LWIwMWQtOTBiN2M4Y2YxZTU3In0.b0o0nn0_9keBgof8u9Scy_7Ysec0hQu_K74SbVs112o"
   var url = 'https://topups-sandbox.reloadly.com/operators/auto-detect/phone/'+phone+'/countries/IN?&includeBundles=true';
 
 
@@ -59,11 +59,6 @@ if(token)
     });
 
 }
-
-
-
-
-
         } catch (err) {
             console.log(err.message);
             res.status(500).send("Error in Saving");
@@ -89,9 +84,19 @@ let plans = await PlansSchema.find({
   operator
 });
 
-res.status(200).json({
-      Data : {plans},Message:"Recharge success",Status:"1000",Token:"tokkens"
-});
+if(plans)
+{
+  res.status(200).json({
+        Data : {plans},Message:"success",Status:"1000",Token:"tokkens"
+  });
+
+}
+else{
+  res.status(200).json({
+        Data : {plans},Message:"no plans found",Status:"1001",Token:"tokkens"
+  });
+
+}
         } catch (err) {
             console.log(err.message);
             res.status(500).send("Error in Saving");
@@ -103,7 +108,6 @@ res.status(200).json({
 router.post(
     "/addPlans",
     [
-
     ],
     async (req, res) => {
         try {
@@ -117,13 +121,11 @@ router.post(
 var plans = new PlansSchema({
   Price,operator,Description,Validity,Talktime,Plan_type
 });
-
 await plans.save();
-
-
 res.status(200).json({
       Data : {plans},Message:"operator added success",Status:"1000",Token:"tokkens"
 });
+
         } catch (err) {
             console.log(err.message);
             res.status(500).send("Error in Saving");
